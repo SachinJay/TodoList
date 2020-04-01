0e0e0447cf5b53db3e5430c226d233ef3054fcf1
Sachin Devasahayam <sjd232@cornell.edu>
2020-04-01 14:14:50 -0400

    Created utils class with convenience methods for manipulating dates

---
+++ src/utils/JDBCUtils.java
@@ -0,0 +1,64 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+//Class with some convenience methods
+
+public class JDBCUtils
+{
+	private static String url = "jdbc:mysql://localhost:3306/demo";
+	private static String user = "root";
+	private static String pass = "y%4lHP$3v0b1%#EJl3BAFuAAeT&T";
+
+	public static Connection getConnection()
+	{
+		Connection connection = null;
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(url, user, pass);
+		} catch (SQLException e)
+		{
+			e.printStackTrace();
+		} catch (ClassNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		return connection;
+	}
+
+	public static void printSQLException(SQLException ex)
+	{
+		for (Throwable e : ex)
+		{
+			if (e instanceof SQLException)
+			{
+				e.printStackTrace(System.err);
+				System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+				System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+				System.err.println("Message: " + e.getMessage());
+				Throwable t = ex.getCause();
+				while (t != null)
+				{
+					System.out.println("Cause: " + t);
+					t = t.getCause();
+				}
+			}
+		}
+	}
+	
+	public static Date getSQLDate(LocalDate date)
+	{
+		return Date.valueOf(date);
+	}
+
+	public static LocalDate getUtilDate(Date sqlDate)
+	{
+		return sqlDate.toLocalDate();
+	}
+
+}
