069f2047e1fee84d97aed6d06ae09aa5b61ce851
Sachin Devasahayam <sjd232@cornell.edu>
2020-04-01 15:26:12 -0400

    Implemented a Dao for the user
    -inserts the user into the user database

---
+++ src/dao/UserDao.java
@@ -0,0 +1,41 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import model.User;
+import utils.JDBCUtils;
+
+public class UserDao
+{
+	public int registerUser(User user)
+	{
+		String insert_users_query = "INSERT INTO users (first_name, last_name, user_name, password)"
+				+ " VALUES (?, ?, ?, ?);";
+		int result = 0; 
+		
+		Connection connection = JDBCUtils.getConnection();
+		
+		try
+		{
+			PreparedStatement ps = connection.prepareStatement(insert_users_query);
+			
+			//Replaces the wildcards we had before
+			ps.setString(1, user.getFirstName());
+			ps.setString(2, user.getLastName());
+			ps.setString(3, user.getUserName());
+			ps.setString(4, user.getPassword());
+			
+			System.out.println(ps);
+			
+			result = ps.executeUpdate();
+		} catch (SQLException e)
+		{
+			JDBCUtils.printSQLException(e);
+		}
+		
+		return result;
+	}
+
+}
