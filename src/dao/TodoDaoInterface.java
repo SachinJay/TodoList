e37fca18aa5252828ee6118ec77c38c5311fecb9
Sachin Devasahayam <sjd232@cornell.edu>
2020-04-03 20:46:23 -0400

    Added comments to mark the parts of CRUD

--- src/dao/TodoDaoInterface.java
+++ src/dao/TodoDaoInterface.java
@@ -6,34 +6,34 @@ import java.util.ArrayList;
 import model.Todo;
 /**
  * Interface to make sure the dao implementes the CRUD operations
- * @author sachi
+ * @author Sachin Devasahayam
  *
  */
 public abstract class TodoDaoInterface
 {
 	
 	/**
-	 * Insert todo into database
+	 * Insert todo into database (C)
 	 * @param todo
 	 * @throws SQLException
 	 */
 	public abstract void insert(Todo todo) throws SQLException;
 	
 	/**
-	 * selects and returns a todo from database
+	 * selects and returns a todo from database (R)
 	 * @param id
 	 * @return
 	 */
 	public abstract Todo select(long id);
 	
 	/**
-	 * Selects all todos
+	 * Selects all todos (R)
 	 * @return
 	 */
 	public abstract ArrayList<Todo> selectAll();
 	
 	/**
-	 * updates a todo, so maybe a field changed or something, this updates it
+	 * updates a todo, so maybe a field changed or something, this updates it (U)
 	 * @param todo
 	 * @return
 	 * @throws SQLException
@@ -41,7 +41,7 @@ public abstract class TodoDaoInterface
 	public abstract Boolean update(Todo todo) throws SQLException;
 	
 	/**
-	 * deletes a todo
+	 * deletes a todo (D)
 	 * @param id
 	 * @return
 	 * @throws SQLException
